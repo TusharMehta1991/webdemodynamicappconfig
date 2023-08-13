33a92733ae9026cd529691f430a1f47658ee7a91
TusharMehta1991 <tusharmehta1991@outlook.com>
2023-08-13 15:21:28 +0530

    initial commit

---
+++ Model/AppSetting.cs
@@ -0,0 +1,7 @@
+namespace webdemodynamicappconfig.Model
+{
+    public class AppSetting{
+        public string BgColor{get;set;}
+        public string HeaderContent{get;set;}
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using webdemodynamicappconfig.Model;
+
+namespace webdemodynamicappconfig.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public AppSetting Settings { get; }
+
+    public IndexModel(ILogger<IndexModel> logger, IOptions<AppSetting> options)
+    {
+        _logger = logger;
+        Settings = options.Value;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
